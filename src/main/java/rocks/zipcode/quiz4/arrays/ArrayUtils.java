f117c53719666897b2aa72a41771c23631b24b60
Zeth Kane <zkane@zipcodes-MBP-9.local>
2020-03-22 13:36:57 -0400

    finshed array utils and all but one string utils

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -5,18 +5,34 @@ package rocks.zipcode.quiz4.arrays;
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        int middle = values.length / 2;
+        return values[middle];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+       int middle = values.length / 2;
+        String[] newArr = new String[values.length - 1];
+        for (int i = 0; i < newArr.length ; i++) {
+            if (i < middle){
+                newArr[i] = values[i];
+            }
+            if (i >= middle){
+                newArr[i] = values[i+1];
+
+            }
+        }
+        return newArr;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        String[] newArr = new String[values.length - 1];
+        for (int i = 0; i < newArr.length ; i++) {
+            newArr[i] = values[i];
+        }
+        return newArr;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,5 +1,9 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author leon on 21/12/2018.
  */
@@ -9,26 +13,70 @@ public class StringUtils {
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        String result = "";
+        StringBuffer stringBuffer = new StringBuffer(str);
+        char ch = Character.toUpperCase(stringBuffer.charAt(str.length()/2));
+        stringBuffer.setCharAt(str.length()/2, ch);
+        result += stringBuffer;
+        return result;
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        String result = "";
+        StringBuffer stringBuffer = new StringBuffer(str);
+        char ch = Character.toLowerCase(stringBuffer.charAt(str.length()/2));
+        stringBuffer.setCharAt(str.length()/2, ch);
+        result += stringBuffer;
+        return result;
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        String[] ary = str.split("");
+        Set<String> mySet = new HashSet<>(Arrays.asList(ary));
+
+        if (str.length() == mySet.size()){
+            return true;
+        }
+       else return false;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i+1)){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        String result = "";
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i+1)){
+                i++;
+            }
+            else result += str.charAt(i);
+        }
+        return result;
     }
 
     public static String invertCasing(String str) {
-        return null;
+        String result = "";
+        char toUpper;
+        char toLower;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) < 91 & str.charAt(i) > 64){
+                toLower = Character.toLowerCase(str.charAt(i));
+                result += toLower;
+            }
+            if (str.charAt(i) < 123 & str.charAt(i) > 96){
+                toUpper = Character.toUpperCase(str.charAt(i));
+                result += toUpper;
+            }
+            if (str.charAt(i) > 31 & str.charAt(i) < 65 || str.charAt(i) == 94){
+                result += str.charAt(i);
+            }
+        }
+        return result;
     }
 }
